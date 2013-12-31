b0354117741c45e16e29189182cb75d5feca89ab
Ian Norton <inorton@gmail.com>
2013-12-31 11:12:12 +0000

    active links

---
+++ KInstrumentsService/KIWebContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XR.Include;
+
+namespace KInstrumentsService
+{
+    public class KIWebContext : BaseContext
+    {
+        public string PagePath { get; set; }
+
+        public string IsActive(string arg)
+        {
+            if (arg == PagePath)
+            {
+                return "active";
+            }
+            return string.Empty;
+        }
+
+
+    }
+}
