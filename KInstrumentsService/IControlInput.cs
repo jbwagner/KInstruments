bb0aee4d52f6be06eb49086bd7bb2437a5cb355c
Ian Norton <inorton@gmail.com>
2014-01-07 07:16:06 +0000

    update htmls

---
+++ KInstrumentsService/IControlInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KInstrumentsService
+{
+    public enum ItemState {
+        Enabled,
+        Disabled,
+        Damaged,
+        Destroyed
+    }
+
+    public interface IControlInput
+    {
+        ItemState ToggleGear();
+        void ToggleStage();
+    }
+}
---
+++ kinstruments/ConcreteControlInput.cs
@@ -0,0 +1,22 @@
+﻿using KInstrumentsService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kinstruments
+{
+    internal class ConcreteControlInput : IControlInput
+    {
+
+        public ItemState ToggleGear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToggleStage()
+        {
+
+        }
+    }
+}
